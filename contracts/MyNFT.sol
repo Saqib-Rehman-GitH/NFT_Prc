e21a51ad0b1e6a2962893152c8ce84116ba35a70
Saqib Rehman <saqibrehman@Saqibs-MacBook-Pro.local>
2022-03-17 17:41:13 +0500

    First Nft Project Setups

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import '@openzepplin/contracts/token/ERC721/ERC721.sol'
+import '@openzepplin/contracts/utils/Counters.sol'
+import '@openzepplin/contracts/access/Ownable.sol'
+import '@openzepplin/contracts/token/ERC721/extensions/ERC721URIStorage.sol'
+
+// - LIBRARIES 
+// 1-dotenv -> connect Metamask And Alchemy.    
+
+
+contract MyNFT is ERC721URIStorage , Ownable {
+
+    using Counters from Counters.Counter;
+    Counters.Counter private _tokenIds;
+    constructor()ERC721("Saqib NFT","S NFT"){
+
+    }
+
+
+    //MINTING -> Creating NFT.
+    function mintNFT(address recepient , string memory tokenURI) public onlyOwner return(uint256)
+    {   
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(recepient,newItemId); // api given by PROTOCOL ERC271 
+        _setToeknURI(newItemId,tokenURI); // api Given by PROTOCOL ERC271.
+        return newItemId;
+    }
+}
