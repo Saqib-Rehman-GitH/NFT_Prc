f78914b19039397a0177e1adc64079e85ebc5ef2
Saqib Rehman <saqibrehman@Saqibs-MacBook-Pro.local>
2022-03-22 11:45:19 +0500

    Compiled First Contract

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
 
-import '@openzepplin/contracts/token/ERC721/ERC721.sol'
-import '@openzepplin/contracts/utils/Counters.sol'
-import '@openzepplin/contracts/access/Ownable.sol'
-import '@openzepplin/contracts/token/ERC721/extensions/ERC721URIStorage.sol'
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 
 // - LIBRARIES 
 // 1-dotenv -> connect Metamask And Alchemy.    
@@ -12,7 +13,7 @@ import '@openzepplin/contracts/token/ERC721/extensions/ERC721URIStorage.sol'
 
 contract MyNFT is ERC721URIStorage , Ownable {
 
-    using Counters from Counters.Counter;
+    using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     constructor()ERC721("Saqib NFT","S NFT"){
 
@@ -20,12 +21,13 @@ contract MyNFT is ERC721URIStorage , Ownable {
 
 
     //MINTING -> Creating NFT.
-    function mintNFT(address recepient , string memory tokenURI) public onlyOwner return(uint256)
+    function mintNFT(address recepient , string memory tokenURI) public onlyOwner returns(uint256) 
     {   
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _mint(recepient,newItemId); // api given by PROTOCOL ERC271 
-        _setToeknURI(newItemId,tokenURI); // api Given by PROTOCOL ERC271.
+        _setTokenURI(newItemId, tokenURI);
+        // _setToeknURI(newItemId,tokenURI); // api Given by PROTOCOL ERC271.
         return newItemId;
     }
 }
